385eeb8573899a7b6e6d40de4de4a2d0987efa7a
VikingFishBird <oybas@hotmail.com>
2020-11-20 22:50:19 -0800

    Machine Learning pt1

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ import pandas as pd
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import sklearn
+from sklearn import linear_model
+import pickle
+
+# Python: 3.8.6
+# scipy: 1.5.4
+# numpy: 1.18.5
+# matplotlib: 3.3.2
+# pandas: 1.1.4
+# sklearn: 0.23.2
 
 # What best projects a receiver's fantasy output?
 # Targets
@@ -18,7 +28,6 @@ years = np.arange(1999, 2020, 1)
 assessment_weeks = np.arange(1, 7, 1)
 judgement_weeks = np.arange(7, 18, 1)
 
-YEAR_TEMP = 2019
 wr_assessment_df = None
 wr_judgement_df = None
 wr_full_data = None
@@ -52,41 +61,95 @@ def get_wr_aggregates(wr_df):
     wr_data_aggregates = wr_data_aggregates[wr_data_aggregates.get('gp') >= 5]
     return wr_data_aggregates
 
-# Create dataframe with wr fantasy averages through first x weeks.
-for week in assessment_weeks:
-    wr_data_weekly = get_wr_df(week, YEAR_TEMP)
+def strip_player_name(name):
+    return name.replace(' ', '').replace('-', '').replace('.', '')
 
-    if wr_assessment_df is None:
-        wr_assessment_df = wr_data_weekly
+for year in years:
+    # Create dataframe with wr fantasy averages through first x weeks.
+    for week in assessment_weeks:
+        print('{} | Week {}'.format(year, week))
+        wr_data_weekly = get_wr_df(week, year)
+
+        if wr_assessment_df is None:
+            wr_assessment_df = wr_data_weekly
+        else:
+            wr_assessment_df = pd.concat([wr_assessment_df, wr_data_weekly])
+
+    wr_assessment_df = get_wr_aggregates(wr_assessment_df)
+
+    # Create dataframe with wr fantasy averages during last x weeks.
+    for week in judgement_weeks:
+        print('{} | Week {}'.format(year, week))
+        wr_data_weekly = get_wr_df(week, year)
+
+        if wr_judgement_df is None:
+            wr_judgement_df = wr_data_weekly
+        else:
+            wr_judgement_df = pd.concat([wr_judgement_df, wr_data_weekly])
+
+    wr_judgement_df = get_wr_aggregates(wr_judgement_df)
+
+    # Join tables for evaluation.
+    wr_year_full_data = wr_assessment_df.assign(Judge_Std=wr_judgement_df.get('Standard'),
+                                                Judge_Half=wr_judgement_df.get('HalfPPR'),
+                                                Judge_PPR=wr_judgement_df.get('FullPPR'))
+    wr_year_full_data = wr_year_full_data[wr_year_full_data.get('Judge_Std').notna()]
+    wr_year_full_data = wr_year_full_data.assign(year=year*np.ones(wr_year_full_data.shape[0]))
+    wr_year_full_data = wr_year_full_data.reset_index()
+    wr_year_full_data = wr_year_full_data.assign(id=wr_year_full_data.get('Player').apply(strip_player_name) +
+                                                    wr_year_full_data.get('year').apply(int).apply(str))
+    wr_year_full_data = wr_year_full_data.set_index('id')
+    if wr_full_data is None:
+        wr_full_data = wr_year_full_data
     else:
-        wr_assessment_df = pd.concat([wr_assessment_df, wr_data_weekly])
-
-wr_assessment_df = get_wr_aggregates(wr_assessment_df)
-
-# Create dataframe with wr fantasy averages during last x weeks.
-for week in judgement_weeks:
-    wr_data_weekly = get_wr_df(week, YEAR_TEMP)
-
-    if wr_judgement_df is None:
-        wr_judgement_df = wr_data_weekly
-    else:
-        wr_judgement_df = pd.concat([wr_judgement_df, wr_data_weekly])
-
-wr_judgement_df = get_wr_aggregates(wr_judgement_df)
-
-# Join tables for evaluation.
-wr_full_data = wr_assessment_df.assign(Judge_Std=wr_judgement_df.get('Standard'),
-                                       Judge_Half=wr_judgement_df.get('HalfPPR'),
-                                       Judge_PPR=wr_judgement_df.get('FullPPR'))
-wr_full_data = wr_full_data[wr_full_data.get('Judge_Std').notna()]
+        wr_full_data = pd.concat([wr_full_data, wr_year_full_data])
 
 print(wr_full_data)
 print(wr_full_data.columns)
+print(wr_full_data.describe())
+
+X = np.array(wr_full_data.get(['Rec_pg', 'Tgt_pg', 'Yds_pg', 'Standard', 'HalfPPR', 'FullPPR']))
+y = np.array(wr_full_data.get(['Judge_Std', 'Judge_Half', 'Judge_PPR']))
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.2)
+
+'''
+# Find Good model.
+best_acc = 0
+for _ in range(50):
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.3)
+    linear = linear_model.LinearRegression()
+
+    linear.fit(x_train, y_train)
+    acc = linear.score(x_test, y_test)
+    print(acc)
+    if acc > best_acc:
+        best_acc = acc
+        with open('wr_fantasy_model.pickle', 'wb') as f:
+            pickle.dump(linear, f)
+print(best_acc)
+'''
+
+pickle_in = open('wr_fantasy_model.pickle', 'rb')
+linear = pickle.load(pickle_in)
+print(linear.score(x_test, y_test))
+
+predictions = linear.predict(x_test)
+for x in range(len(predictions)):
+    print(predictions[x], x_test[x], y_test[x])
+
 
 # Plots!
-wr_full_data.plot(x='Rec_pg', y='Judge_Std', kind='scatter')
+wr_full_data.plot(x='Rec_pg', y='Judge_Std', kind='scatter', alpha=0.5)
+plt.title('Rec_pg vs Judge_Std', fontname='DejaVu Sans', fontsize=18)
+wr_full_data.plot(x='Tgt_pg', y='Judge_Std', kind='scatter', alpha=0.5)
+plt.title('Tgt_pg vs Judge_Std', fontname='DejaVu Sans', fontsize=18)
+wr_full_data.plot(x='Yds_pg', y='Judge_Std', kind='scatter', alpha=0.5)
+plt.title('Yds_pg vs Judge_Std', fontname='DejaVu Sans', fontsize=18)
 
-plt.show()
+
+
+
+#plt.show()
 
 
 
